2f44a636e779a35575147eebdf81e63998be8a55
Francisco Orquera Lorda <forqueral@fi.uba.ar>
2024-02-15 16:05:25 -0300

    sign up form

--- app/page.tsx
+++ app/page.tsx
@@ -24,12 +24,21 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+          <div className='flex flex-row'>
+            <Link
+              href="/signup"
+              className="mr-4 flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            >
+              <span>Sign Up</span> <ArrowRightIcon className="w-5 md:w-6" />
+            </Link>
+            
+            <Link
+              href="/login"
+              className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            >
+              <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            </Link>
+          </div>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           <Image src='/hero-desktop.png' width={1000} height={760} className="hidden md:block" alt="Screenshots of the dashboard project showing desktop version"/>
--- app/ui/signup-form.tsx
+++ app/ui/signup-form.tsx
@@ -3,6 +3,7 @@
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
+  UserCircleIcon,
   KeyIcon,
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
@@ -12,16 +13,35 @@ import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '@/app/lib/actions';
 import Link from 'next/link';
 
-export default function LoginForm() {
+export default function SignUpForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
   return (
     <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
+          Create your free account
         </h1>
         <div className="w-full">
+        <div>
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="email"
+            >
+              Username
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Enter your username"
+                required
+              />
+              <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+          </div>
           <div>
             <label
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
@@ -66,17 +86,17 @@ export default function LoginForm() {
             <span
                   className="mb-3 mt-5 block text-xs font-medium text-gray-900"
             >
-              Don't have an account yet? &nbsp;
+              Already have an account? &nbsp;
             </span>
-          <Link href="/signup">
+          <Link href="/login">
             <span
                   className="mb-3 mt-5 block text-xs font-medium text-gray-900 hover:text-blue-600"
                 >
-                  Sign Up
+                  Login
             </span>
           </Link>
         </div>
-        <LoginButton />
+        <SignUpButton />
         <div className="flex h-8 items-end space-x-1">
           {errorMessage && (
               <>
@@ -90,12 +110,12 @@ export default function LoginForm() {
   );
 }
 
-function LoginButton() {
+function SignUpButton() {
   const { pending } = useFormStatus();
  
   return (
     <Button className="mt-4 w-full" aria-disabled={pending}>
-      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+      Sign Up <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   );
 }
